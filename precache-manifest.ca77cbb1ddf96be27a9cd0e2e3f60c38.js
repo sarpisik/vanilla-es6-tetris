59969fae28cbbf3cd11537231c57ca95ebee7c5d
sarpisik <contact@sarpisik.com>
2019-09-10 19:17:04 +0300

    Updates

---
+++ precache-manifest.ca77cbb1ddf96be27a9cd0e2e3f60c38.js
@@ -0,0 +1,48 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf91eb4b5e83f4a42b27",
+    "url": "main.6177d8003b2e2f300e66.css"
+  },
+  {
+    "revision": "bf91eb4b5e83f4a42b27",
+    "url": "main.1947a912896fd7f2fd47.js"
+  },
+  {
+    "revision": "abd3f9d41a3e409a6fa4",
+    "url": "runtime.9a485b6d824d6965812c.js"
+  },
+  {
+    "url": "images/99e4ba03b388f9307396088033f91217.png"
+  },
+  {
+    "url": "images/14885f50f2b7d7fdb3bab7b1b8bbc528.png"
+  },
+  {
+    "url": "images/f3ef1c05279bda5f921e2e22a8129179.png"
+  },
+  {
+    "url": "fonts/2479d9179eb53a1211fa8a6bb10b9f9b.eot"
+  },
+  {
+    "url": "fonts/af28bb8890fd35ab2c6f8c16ae16de03.woff2"
+  },
+  {
+    "url": "fonts/eb54a705d8ad04a0ab6a79e5fb4ee4e8.woff"
+  },
+  {
+    "url": "images/3c51cf95e0cf5b2145510b36882a66ed.svg"
+  },
+  {
+    "url": "fonts/cc5aa8a105931243a9c12221ded96383.svg"
+  },
+  {
+    "url": "fonts/dde09f55e9a05ead266ca2cfef7dacfc.ttf"
+  },
+  {
+    "revision": "5aa22c5f4aca8edfa27241bd0fdf755c",
+    "url": "index.html"
+  },
+  {
+    "url": "manifest.4bbd92dc6aa7257e9a34d408590c5526.json"
+  }
+]);
