5f174d795ec194ddba9a9523776f54644066e620
sarpisik <contact@sarpisik.com>
2019-09-12 21:46:11 +0300

    Edit pwa config

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV !== 'production') {
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
       navigator.serviceWorker
-        .register('/service-worker.js')
+        .register('/vanilla-es6-tetris/service-worker.js')
         .then(registration => {
           console.log('SW registered: ', registration);
         })
